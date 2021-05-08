33b8709e7c7da0135590ffdf74f68127afc82630
fdbva <villela.felipe@gmail.com>
2021-05-07 22:12:18 -0300

    Add project files.

---
+++ Aula06/Controllers/StringController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Aula06.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula06.Controllers
+{
+    public class StringController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Concat(StringInputModel stringInputModel)
+        {
+            var result = stringInputModel.Left + stringInputModel.Right;
+            stringInputModel.Result = result;
+
+            return View("Index", stringInputModel);
+        }
+
+        [HttpPost]
+        public IActionResult Compare(StringInputModel stringInputModel)
+        {
+            var result = string.Equals(stringInputModel.Left, stringInputModel.Right, StringComparison.OrdinalIgnoreCase);
+
+            stringInputModel.Result = result.ToString();
+
+            return View("Index", stringInputModel);
+        }
+    }
+}
---
+++ Aula06/Models/StringInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Aula06.Models
+{
+    public class StringInputModel
+    {
+        public string Left { get; set; }
+        public string Right { get; set; }
+        public string Result { get; set; }
+    }
+}
